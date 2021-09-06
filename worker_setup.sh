885d55e8b6352e6b1bacfe9b5d2f7a082189d38e
Alexey M <mendic9c@gmail.com>
2021-09-06 15:09:41 -0400

    comment out Status
    had to comment out the status line because it was stopping the work flow

--- worker_setup.sh
+++ worker_setup.sh
@@ -3,6 +3,6 @@ sudo apt-get install -y kubelet kubeadm kubectl
 sudo systemctl daemon-reload
 sudo systemctl enable kubelet
 sudo systemctl start kubelet
-sudo systemctl status kubelet
+#sudo systemctl status kubelet
 
 sudo kubeadm join 172.31.15.32:6443 --token w99put.wv8c2vig6t9z6aqb --discovery-token-ca-cert-hash sha256:c14f0002c7a341638d48be8dc6e9875d1861f590c2fec8a329ffa0ebb70d9d54

0ee2b7f1052c975fa025896fbc388a726cc31fcd
Alexey M <mendic9c@gmail.com>
2021-09-06 13:52:32 -0400

    Create master_setup.sh
    initial script for kubernetes master

---
+++ master_setup.sh
@@ -0,0 +1,8 @@
+sudo apt-get install -y kubelet kubeadm kubectl
+
+sudo systemctl daemon-reload
+sudo systemctl enable kubelet
+sudo systemctl start kubelet
+sudo systemctl status kubelet
+
+sudo kubeadm init

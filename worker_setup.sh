43245ab738c5919e4665e7a6a51ba0f18b15144a
Alexey M <mendic9c@gmail.com>
2021-09-06 15:07:26 -0400

    forgot sudo

--- worker_setup.sh
+++ worker_setup.sh
@@ -5,4 +5,4 @@ sudo systemctl enable kubelet
 sudo systemctl start kubelet
 sudo systemctl status kubelet
 
-kubeadm join 172.31.15.32:6443 --token w99put.wv8c2vig6t9z6aqb --discovery-token-ca-cert-hash sha256:c14f0002c7a341638d48be8dc6e9875d1861f590c2fec8a329ffa0ebb70d9d54
+sudo kubeadm join 172.31.15.32:6443 --token w99put.wv8c2vig6t9z6aqb --discovery-token-ca-cert-hash sha256:c14f0002c7a341638d48be8dc6e9875d1861f590c2fec8a329ffa0ebb70d9d54
